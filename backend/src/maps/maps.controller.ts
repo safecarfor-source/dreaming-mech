27a60cf519c483907d78748009c9a4524474b5ac
horoong <jinhyung3012@gmail.com>
2026-02-03 13:09:19 +0900

    feat:maps dto 추가

--- backend/src/maps/maps.controller.ts
+++ backend/src/maps/maps.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { MapsService } from './maps.service';
+import { GeocodeDto } from './dto/geocode.dto';
+import { ReverseGeocodeDto } from './dto/reverse-geocode.dto';
 
 @Controller('maps')
 export class MapsController {
@@ -7,25 +9,13 @@ export class MapsController {
 
   // GET /maps/geocode?address=서울시 강남구 테헤란로 123
   @Get('geocode')
-  async geocode(@Query('address') address: string) {
-    if (!address) {
-      throw new BadRequestException('주소를 입력해주세요');
-    }
-    return await this.mapsService.geocode(address);
+  async geocode(@Query() query: GeocodeDto) {
+    return await this.mapsService.geocode(query.address);
   }
 
   // GET /maps/reverse?lat=37.5&lng=127.0
   @Get('reverse')
-  async reverseGeocode(
-    @Query('lat') lat: string,
-    @Query('lng') lng: string,
-  ) {
-    if (!lat || !lng) {
-      throw new BadRequestException('위도와 경도를 입력해주세요');
-    }
-    return await this.mapsService.reverseGeocode(
-      parseFloat(lat),
-      parseFloat(lng),
-    );
+  async reverseGeocode(@Query() query: ReverseGeocodeDto) {
+    return await this.mapsService.reverseGeocode(query.lat, query.lng);
   }
 }
